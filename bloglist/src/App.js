e12041c6cb627bef806e7aa54d776a214fbc93fd
Victor Molero <victor.molero@gmail.com>
2019-12-30 19:01:03 +0100

    useResource + prettier --sigle-quote --semi=false

--- bloglist/src/App.js
+++ bloglist/src/App.js
@@ -1,145 +1,163 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import loginService from './services/loginService';
-import blogService from './services/blogService';
-import LoginForm from './components/LoginForm';
-import Logout from './components/Logout';
-import BlogList from './components/BlogList';
-import CreateBlogForm from './components/CreateBlogForm';
-import Toast from './components/Toast';
-import Togglable from './components/Toggable';
-import { useResource } from './hooks';
+import React, { useState, useEffect } from 'react'
+import loginService from './services/loginService'
+import blogService from './services/blogService'
+import LoginForm from './components/LoginForm'
+import Logout from './components/Logout'
+import BlogList from './components/BlogList'
+import CreateBlogForm from './components/CreateBlogForm'
+import Toast from './components/Toast'
+import Togglable from './components/Toggable'
 
-import './App.css';
+import './App.css'
 
-function findBlogWithId(blogId, blogs) {
+function findBlogPositionWithId(blogId, blogs) {
   let positionInArray = -1,
     found = false,
-    end = false;
+    end = false
 
   do {
-    positionInArray += 1;
-    found = blogs[positionInArray].id === blogId;
-    end = positionInArray === blogs.length - 1;
-  } while (!found && !end);
+    positionInArray += 1
+    found = blogs[positionInArray].id === blogId
+    end = positionInArray === blogs.length - 1
+  } while (!found && !end)
 
-  return blogs[positionInArray];
+  return positionInArray
+}
+
+function extractElementAt(positionInArray, blogs) {
+  let blogToUpdate = null
+  const blogsCopy = blogs.map(blog => blog)
+  if (positionInArray > -1) {
+    blogToUpdate = blogsCopy.splice(positionInArray, 1).pop()
+  }
+
+  return [blogToUpdate, blogsCopy]
+}
+
+function insertElementAt(positionInArray, blogsCopy, updatedBlog) {
+  blogsCopy.splice(positionInArray, 0, updatedBlog)
 }
 
 function sortByLikes(blogsCopy) {
   blogsCopy.sort((blogA, blogB) =>
     parseInt(blogA.likes) > parseInt(blogB.likes) ? -1 : 1
-  );
+  )
 }
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [blogs, blogResource] = useResource('/api/blogs');
-  const [renderBlogs, setRenderBlogs] = useState([]);
-  const [toast, setToast] = useState({ type: '', text: '' });
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+  const [toast, setToast] = useState({ type: '', text: '' })
 
   const showToast = (text, type = 'success') => {
-    setToast({ type, text });
+    setToast({ type, text })
     setTimeout(() => {
-      setToast({ type: '', text: '' });
-    }, 5000);
-  };
+      setToast({ type: '', text: '' })
+    }, 5000)
+  }
+
+  const getBlogList = async user => {
+    try {
+      const blogList = await blogService.getAll(user.token)
+      sortByLikes(blogList)
+      setBlogs(blogList)
+    } catch (err) {
+      showToast('Failed to get the list of blogs', 'error')
+    }
+  }
 
   const handleLogin = ({ username, password }) => async event => {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      const loggedUser = await loginService.login({ username, password });
+      const loggedUser = await loginService.login({ username, password })
       if (!('token' in loggedUser)) {
-        throw new Error('Token not found');
+        throw new Error('Token not found')
       }
-      setUser(loggedUser);
-      window.localStorage.setItem('login', JSON.stringify(loggedUser));
-      showToast('Successfully logged in as ' + username);
-      await getBlogList(loggedUser);
+      setUser(loggedUser)
+      window.localStorage.setItem('login', JSON.stringify(loggedUser))
+      showToast('Successfully logged in as ' + username)
+      await getBlogList(loggedUser)
     } catch (err) {
-      showToast('Failed to log in', 'error');
+      showToast('Failed to log in', 'error')
     }
-  };
+  }
 
   const handleLogout = () => {
     try {
-      window.localStorage.removeItem('login');
-      setUser(null);
-      showToast('Successfully logged out');
+      window.localStorage.removeItem('login')
+      setUser(null)
+      setBlogs([])
+      showToast('Successfully logged out')
     } catch (err) {
-      showToast('Failed to log out', 'error');
+      showToast('Failed to log out', 'error')
     }
-  };
+  }
 
   const handleCreateBlog = ({ author, title, url }) => async event => {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      await blogResource.create({ author, title, url }, user.token);
-      showToast(`Blog entry ${title} by ${author} created successfully`);
+      await blogService.create({ author, title, url }, user.token)
+      await getBlogList(user)
+      showToast(`Blog entry ${title} by ${author} created successfully`)
     } catch (err) {
-      showToast(`Failed to create ${title} by ${author}`, 'error');
+      showToast(`Failed to create ${title} by ${author}`, 'error')
     }
-  };
+  }
 
   const handleLike = blogId => async event => {
-    event.preventDefault();
+    event.preventDefault()
     try {
-      const blogToUpdate = findBlogWithId(blogId, blogs);
-      const newLikesValue = blogToUpdate.likes + 1;
-      await blogResource.update(
+      const positionInArray = findBlogPositionWithId(blogId, blogs)
+      const [blogToUpdate, blogsCopy] = extractElementAt(positionInArray, blogs)
+      const newLikesValue = blogToUpdate.likes + 1
+      const updatedBlog = await blogService.update(
         { ...blogToUpdate, likes: newLikesValue },
         user.token
-      );
-
+      )
+      updatedBlog.user = blogToUpdate.user
+      insertElementAt(positionInArray, blogsCopy, updatedBlog)
+      sortByLikes(blogsCopy)
+      setBlogs(blogsCopy)
       showToast(
         `Blog ${blogToUpdate.title} written by ${blogToUpdate.author} liked!`
-      );
+      )
     } catch (err) {
-      showToast('Failed to update blog', 'error');
+      showToast('Failed to update blog', 'error')
     }
-  };
+  }
 
   const handleDelete = blogId => async event => {
-    event.preventDefault();
-    const blogToDelete = findBlogWithId(blogId, blogs);
+    event.preventDefault()
+    const positionInArray = findBlogPositionWithId(blogId, blogs)
+    const [blogToDelete, blogsCopy] = extractElementAt(positionInArray, blogs)
     if (
       window.confirm(
         `Remove blog ${blogToDelete.title} by ${blogToDelete.author}?`
       )
     ) {
       try {
-        await blogService.delete(blogToDelete, user.token);
-        await getBlogList(user);
-        showToast(`Blog entry ${blogToDelete.title} deleted`);
+        await blogService.delete(blogToDelete, user.token)
+        setBlogs(blogsCopy)
+        showToast(`Blog entry ${blogToDelete.title} deleted`)
       } catch (err) {
-        showToast('Failed to delete blog', 'error');
+        showToast('Failed to delete blog', 'error')
       }
     }
-  };
+  }
 
   useEffect(() => {
-    sortByLikes(blogs);
-    setRenderBlogs(blogs);
-  }, [blogs]);
-
-  const stableBlogResource = useCallback(blogResource, []);
-  const getBlogList = useCallback(
-    async user => {
-      try {
-        await stableBlogResource.getAll(user.token);
-      } catch (err) {
-        showToast('Failed to get the list of blogs', 'error');
-      }
-    },
-    [stableBlogResource]
-  );
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('login');
+    const getAllBlogs = async user => {
+      const blogList = await blogService.getAll(user.token)
+      sortByLikes(blogList)
+      setBlogs(blogList)
+    }
+    const loggedUserJSON = window.localStorage.getItem('login')
     if (loggedUserJSON) {
-      const savedUser = JSON.parse(loggedUserJSON);
-      setUser(savedUser);
-      getBlogList(savedUser);
+      const savedUser = JSON.parse(loggedUserJSON)
+      setUser(savedUser)
+      getAllBlogs(savedUser)
     }
-  }, [getBlogList]);
+  }, [])
 
   return (
     <>
@@ -155,14 +173,14 @@ const App = () => {
           </Togglable>
           <BlogList
             user={user}
-            blogs={renderBlogs}
+            blogs={blogs}
             handleLike={handleLike}
             handleDelete={handleDelete}
           />
         </>
       )}
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
