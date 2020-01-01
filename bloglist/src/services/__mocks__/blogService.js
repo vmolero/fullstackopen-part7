69313bb2a02733a36aaeaa5f79783187fdb104f3
Victor Molero <victor.molero@gmail.com>
2020-01-01 19:10:00 +0100

    remove entries

--- bloglist/src/services/__mocks__/blogService.js
+++ bloglist/src/services/__mocks__/blogService.js
@@ -1,246 +1,32 @@
-const getAll = () => [
-  {
-    likes: 13,
-    author: 'Michael Night',
-    url: 'http://the-speaking-car.com',
-    title: 'The speaking car',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
+const getAll = async () => {
+  return Promise.resolve([
+    {
+      likes: 13,
+      author: 'Michael Night',
+      url: 'http://the-speaking-car.com',
+      title: 'The speaking car',
+      user: {
+        blogs: [],
+        username: 'victor',
+        name: 'Victor Molero',
+        id: '5dfbfde7caf43d648638e5f1'
+      },
+      id: '5dfe30925e7bba29e581b07d'
     },
-    id: '5dfe30925e7bba29e581b07d'
-  },
-  {
-    likes: 65,
-    author: 'Sponge Bob',
-    url: 'http://living-in-a-pinapple.com',
-    title: 'Living in a pineapple',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe313f5e7bba29e581b07e'
-  },
-  {
-    likes: 0,
-    author: 'Patrick Star',
-    url: 'http://you-are-not-patrick.com',
-    title: 'You are not Patrick',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe3376c1bac82fe42275ce'
-  },
-  {
-    likes: 1,
-    author: 'Patrick Star',
-    url: 'http://dont-eat-its-platic.com',
-    title: "Don't eat, it's plastic",
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe33e7c1bac82fe42275cf'
-  },
-  {
-    likes: 0,
-    author: 'Patrick Star',
-    url: 'http://dont-eat-its-platic.com',
-    title: "Don't eat, it's plastic",
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe3ed75f0a0d48cea1e3c8'
-  },
-  {
-    likes: 7,
-    author: 'Sponge Bob',
-    url: 'http://the-imaginaation.com',
-    title: 'The imaginaation',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe3f683abe2549862cb555'
-  },
-  {
-    likes: 707,
-    title: 'I love lego train',
-    author: 'Daniel Molero',
-    url: 'http://daniel-molero.com/blog/',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe400a3abe2549862cb556'
-  },
-  {
-    likes: 701,
-    title: 'I love lego train',
-    author: 'Daniel Molero',
-    url: 'http://daniel-molero.com/blog/',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dfe40783abe2549862cb557'
-  },
-  {
-    likes: 0,
-    author: 'Patrick Star',
-    url: 'http://i-love-you.com',
-    title: 'I love you',
-    user: {
-      blogs: [],
-      username: 'daniel',
-      name: 'Daniel Molero',
-      id: '5dfd4cdd84cd364601e7530a'
-    },
-    id: '5dfe445c48eb1f5c4bcef9d9'
-  },
-  {
-    likes: 7,
-    author: 'Patrick Star',
-    url: 'http://i-love-you.com',
-    title: 'I love you',
-    user: null,
-    id: '5dfe4aeada06166e817f9f59'
-  },
-  {
-    likes: 0,
-    author: 'Victor Molero',
-    title: 'Pls react ',
-    url: 'http://pls-react.com',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dffbd0d7088a2476349c891'
-  },
-  {
-    likes: 23,
-    author: 'qwe',
-    title: 'qwe',
-    url: 'qwe',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dffc7267088a2476349c894'
-  },
-  {
-    likes: 10,
-    author: 'ahdfd',
-    title: 'hadfhah',
-    url: 'ahhdff',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dffc9177088a2476349c898'
-  },
-  {
-    likes: 40,
-    author: 'Me',
-    title: 'Yo Mio Mio',
-    url: 'http:/yo-mio-mio.com',
-    user: {
-      blogs: [],
-      username: 'victor',
-      name: 'Victor Molero',
-      id: '5dfbfde7caf43d648638e5f1'
-    },
-    id: '5dffc9527088a2476349c899'
-  },
-  {
-    likes: 0,
-    author: 'Calamardo',
-    title: 'Soy un aburrido',
-    url: 'http://soy un aburrido',
-    user: {
-      blogs: [],
-      username: 'daniel',
-      name: 'Daniel Molero',
-      id: '5dfd4cdd84cd364601e7530a'
-    },
-    id: '5e01f872b694467b932abbf4'
-  },
-  {
-    likes: 0,
-    author: 'Calamardo',
-    title: 'Soy patudo',
-    url: 'http://soy-patudo.com',
-    user: {
-      blogs: [],
-      username: 'daniel',
-      name: 'Daniel Molero',
-      id: '5dfd4cdd84cd364601e7530a'
-    },
-    id: '5e01f8c9b694467b932abbf5'
-  },
-  {
-    likes: 8,
-    author: 'Calamardo',
-    title: 'Soy vago',
-    url: 'http://soy-vago',
-    user: {
-      blogs: [],
-      username: 'daniel',
-      name: 'Daniel Molero',
-      id: '5dfd4cdd84cd364601e7530a'
-    },
-    id: '5e01f947b694467b932abbf6'
-  },
-  {
-    likes: 0,
-    author: 'Arenita',
-    title: 'Soy inventora',
-    url: 'http://soy-inventora',
-    user: {
-      blogs: [],
-      username: 'daniel',
-      name: 'Daniel Molero',
-      id: '5dfd4cdd84cd364601e7530a'
-    },
-    id: '5e01f983b694467b932abbf7'
-  },
-  {
-    likes: 1,
-    author: 'asdasd',
-    title: 'asdasd',
-    url: 'adasd',
-    user: {
-      blogs: [],
-      username: 'daniel',
-      name: 'Daniel Molero',
-      id: '5dfd4cdd84cd364601e7530a'
-    },
-    id: '5e01f9f6b694467b932abbf8'
-  }
-]
+    {
+      likes: 65,
+      author: 'Sponge Bob',
+      url: 'http://living-in-a-pinapple.com',
+      title: 'Living in a pineapple',
+      user: {
+        blogs: [],
+        username: 'victor',
+        name: 'Victor Molero',
+        id: '5dfbfde7caf43d648638e5f1'
+      },
+      id: '5dfe313f5e7bba29e581b07e'
+    }
+  ])
+}
 
 export default { getAll }
